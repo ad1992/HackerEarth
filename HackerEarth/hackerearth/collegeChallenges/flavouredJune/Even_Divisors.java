968ba3d3137d2945979910b7b40b02c6fdaa284e
Aakansha <monstershome@gmail.com>
2015-07-26 20:24:12 +0530

    code refactored

--- HackerEarth/hackerearth/collegeChallenges/flavouredJune/Even_Divisors.java
+++ HackerEarth/hackerearth/collegeChallenges/flavouredJune/Even_Divisors.java
@@ -24,44 +24,48 @@ import java.util.Set;
  */
 public class Even_Divisors {
 
-		public static void main(String[] args) throws NumberFormatException, IOException {
-			BufferedReader br=new BufferedReader(new InputStreamReader(System.in)); 
-			
-			int T=Integer.parseInt(br.readLine().trim());
-			
-			while(T-->0)
-			{
-				long N=Long.parseLong(br.readLine().trim());
-				int ans=get_EverDivisor_Count(N);
-	            System.out.println(ans);
-			}
-
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in)); 
+		
+		int T=Integer.parseInt(br.readLine().trim());
+		
+		while(T-->0)
+		{
+			long N=Long.parseLong(br.readLine().trim());
+			int ans=get_EverDivisor_Count(N);
+            System.out.println(ans);
 		}
 
-		private static int get_EverDivisor_Count(long N) {
-	        Set<Long> set=new HashSet<>();
-			long sqrt=(long)Math.ceil((Math.sqrt(N)));
-			long i=2;		
-	        if(N==2)
-	            return 1;
-	        if((N&1)==0)
-	            set.add(N);
-	        
-			while(i<=sqrt)
+	}
+
+	private static int get_EverDivisor_Count(long N) {
+       
+		long sqrt=(long)Math.sqrt(N);
+		long i=2;		
+        int c=0;
+        if(N==2)
+            return 1;
+        if((N&1)==0)
+          c++;   
+        else
+            return 0;
+        
+		while(i<=sqrt)
+		{
+			if(N%i==0)
 			{
-				if(N%i==0)
-				{
-					if((i&1)==0)
-	                    set.add(i);              
-	                long a=N/i;
-	                if(i*i!=N&&(a&1)==0)	
-	                     set.add(a);				
-					//System.out.println(c+" "+i);           
-	              
-						
-				}
-				i++;
+				if((i&1)==0)
+                   c++;              
+                long a=N/i;
+                if(i*i!=N&&(a&1)==0)	
+                   c++;   				
+				//System.out.println(c+" "+i);           
+              
+					
 			}
-			return set.size();
+			i++;
 		}
+		return c;
+	}
+
 }
